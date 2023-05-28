cd69eef12f325365f0844341d49b700b4d2bf1be
DyshliukAnna <113441385+DyshliukAnna@users.noreply.github.com>
2023-05-28 13:32:25 +0300

    prettier

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,15 +1,13 @@
 import Notiflix from 'notiflix';
 const refs = {
-  
- form: document.querySelector(".form"),
- delay: document.querySelector('input[name="delay"]'),
- step: document.querySelector('input[name="step"]'),
+  form: document.querySelector('.form'),
+  delay: document.querySelector('input[name="delay"]'),
+  step: document.querySelector('input[name="step"]'),
   amount: document.querySelector('input[name="amount"]'),
- startButon: document.querySelector('button'),
+  startButon: document.querySelector('button'),
+};
 
-}
-
-refs.startButon.addEventListener("click", returnAmount);
+refs.startButon.addEventListener('click', returnAmount);
 
 function returnAmount(e) {
   e.preventDefault();
@@ -19,16 +17,19 @@ function returnAmount(e) {
   for (let i = 1; i <= amount; i += 1) {
     createPromise(i, firstDelay + delaySteps * (i - 1))
       .then(({ position, delay }) => {
-        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
       })
       .catch(({ position, delay }) => {
-        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
       });
   }
 
   refs.form.reset();
 }
-
 
 function createPromise(position, delay) {
   const promise = new Promise((resolve, reject) => {
