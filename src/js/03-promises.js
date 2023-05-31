b541ccfa18174e7598d9a4abbf5f5cbb81bfb195
DyshliukAnna <113441385+DyshliukAnna@users.noreply.github.com>
2023-05-31 14:17:10 +0300

    add one more exemple

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -44,3 +44,34 @@ function createPromise(position, delay) {
   });
   return promise;
 }
+
+// function returnAmount(e) {
+//   e.preventDefault();
+//   const amount = refs.amount.value;
+//   const firstDelay = Number(refs.delay.value);
+//   let stepDelay = Number(refs.step.value);
+//   for (let i = 1; i <= amount; i += 1) {
+//     createPromise(i, firstDelay + stepDelay * (i - 1))
+//       .then(({ position, delay }) => {
+//         console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//       })
+//       .catch(({ position, delay }) => {
+//         console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//       });
+//   }
+//   refs.form.reset();
+// }
+
+// function createPromise(position, delay) {
+//   const promise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       const shouldResolve = Math.random() > 0.3;
+//       if (shouldResolve) {
+//         resolve({ position, delay });
+//       } else {
+//         reject({ position, delay });
+//       }
+//     }, delay);
+//   });
+//   return promise;
+// }
